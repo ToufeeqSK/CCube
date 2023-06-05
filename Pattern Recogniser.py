20cde1fc93407a08b1283175999af341c07f39db
ToufeeqSK <115100540+ToufeeqSK@users.noreply.github.com>
2023-06-05 17:20:35 +0530

    Pattern Recogniser.py

--- Pattern Recogniser.py
+++ Pattern Recogniser.py
@@ -1,23 +1,4 @@
 #Pattern Recogniser:
-'''num_list_1=[]
-num_list_2=[]
-length=num_list_2
-n=int(input("Enter the range of the list:"))
-for i in range(n):
-    p=int(input())
-    num_list_1.append(p)
-print("The list:",num_list_1)
-for j in num_list_1:
-     if j not in num_list_2:
-        num_list_2.append(j)
-     for i in range(1, length // 2 + 1):
-        pattern = num_list_2[:i]
-        repeats = length // i
-        if pattern * repeats == num_list_1:
-            print("Pattern Found")
-            print("Pattern:", pattern)
-            print("Repeats:", repeats)
-'''
 def find_pattern(string):
     length= len(string)
     
